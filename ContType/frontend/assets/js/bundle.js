80982d0cfb9a444185cb2371bb8c8ed6ed2d48c4
Martiel dos Santos Lima <125515972+MartielLima@users.noreply.github.com>
2023-10-12 13:39:44 -0300

    hjkl

---
+++ ContType/frontend/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./src/webpack/index.ts ***!
+  \******************************/
+
+console.log('Ola mundo');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
